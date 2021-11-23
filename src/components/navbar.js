da93957af1611a8f5e511b93b84ae479de0e28ac
bryassab <brayanruizomg@gmail.com>
2021-11-23 17:59:22 -0500

    viaje 3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 
 
 function Navbar() {
+    const [Sidebar, SetSidebar] =  useState(false)
+    const showSidebar = () => {
+        SetSidebar(!Sidebar)
+     }
     return (
         <>
-        <div className="Navbar">
-            < Link to="#" className='menu.barra'>
-                <FaIcons.FaBars/>
-            </Link>
-            
-        </div>
+            <div className="Navbar">
+                < Link to="#" className='menu-barra'>
+                    <FaIcons.FaBars onClick={showSidebar} />
+                </Link>
+            </div>
+            <nav className={Sidebar ? 'Nav-menu Active' : 'Nav-menu'}>
+                <ul className='Nav-menu-items'>
+                    <li className="Navbar-toggle">
+                        <Link to="#" className='menu-barra'>
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
